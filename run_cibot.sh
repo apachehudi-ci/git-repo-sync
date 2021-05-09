858ebc7df03470ea2759af4a4349f56f52b2a463
Raymond Xu <2701446+xushiyan@users.noreply.github.com>
2021-05-09 10:39:22 -0700

    update settings
    branch sync interval change to 3min
    remove travis token

--- run_cibot.sh
+++ run_cibot.sh
@@ -4,7 +4,6 @@ source ${HOME}/.profile
 
 java -jar ${HOME}/ci-bot/target/ci-bot.jar \
   --azureToken ${AZURE_TOKEN} \
-  --travisToken ${TRAVIS_TOKEN} \
   --ciRepository 'apachehudi-ci/hudi-branch-ci' \
   --githubToken ${GITHUB_TOKEN} \
   --observedRepository 'apache/hudi' --user hudi-bot --interval 0
